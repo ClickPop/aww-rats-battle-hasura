0ba393ec45d4b635f5396a0b52485a92e8c49d96
Graham Vasquez <rescus1221@gmail.com>
2022-02-14 01:24:13 -0500

    Added reset_energy SQL function as mutation, added cron job at midnight

---
+++ hasura/migrations/default/1644818914093_reset_energy_function/down.sql
@@ -0,0 +1,9 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION reset_energy()
+-- RETURNS SETOF players
+-- AS $$
+--   UPDATE players SET energy = max_energy WHERE energy < max_energy RETURNING *
+-- $$
+--  LANGUAGE SQL
+--  VOLATILE;

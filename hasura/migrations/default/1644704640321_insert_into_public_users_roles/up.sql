b43a88c913a233eca86500c1eefa4d3e3b1f522b
Graham Vasquez <rescus1221@gmail.com>
2022-02-13 21:59:37 -0500

    Fixed migrations and actions

--- hasura/migrations/default/1644704640321_insert_into_public_users_roles/up.sql
+++ hasura/migrations/default/1644704640321_insert_into_public_users_roles/up.sql
@@ -1,1 +1,3 @@
+DELETE FROM "public"."users_roles" WHERE "wallet" = '0x38882f37879ae3d53100f90651df448960db4975';
+
 INSERT INTO "public"."users_roles"("wallet", "role") VALUES (E'0x38882F37879aE3d53100F90651Df448960dB4975', E'admin');
